9fd825766b29502647757ccaa01442c2c4f461a5
virajkrishna79 <163158237+virajkrishna79@users.noreply.github.com>
2025-10-21 12:29:11 +0530

    Update __init__.py

--- backend/ml_models/__init__.py
+++ backend/ml_models/__init__.py
@@ -1,6 +1,6 @@
 # ML Models package initialization
 # backend/models/__init__.py
-#from .user import User
+from .user import User
 from .stock_recommendation import StockRecommendation
 from .news_article import NewsArticle
 
